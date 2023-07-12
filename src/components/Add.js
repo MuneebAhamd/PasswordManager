82f306d76843e5254dbf5519bab65b03253c0ebe
MuneebAhmad <gorijutt124@gmail.com>
2023-07-12 14:47:18 +0500

    Some Changes of Add.js Componenet

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,95 +1,67 @@
-import React, { useState, useReducer  , useContext} from 'react'
-import {Button, Form  } from 'react-bootstrap'
-import firebase from 'firebase'
-import {UserContext} from './UserContext'
+import React, { useState, useReducer, useContext } from 'react';
+ import { Button, Form } from 'react-bootstrap';
+  import firebase from 'firebase';
+   import { UserContext } from './UserContext';
 
-function Add() {
+function Add() { let userUID = useContext(UserContext);
+   const [email, setEmail] = useState(''); 
+   const [password, setPassword] = useState('');
 
-    let userUID = useContext(UserContext)
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+const initMessage = { text: '', type: null, };
 
-    const initMessage = {
-        text: '',
-        type: null
-    }
+const reducer = (state, action) =>
+ { switch (action)
+   { case 'success':
+    return { text: 'Added Successfully'
+    , type: 'success', };
+     case 'null_password'
+     : 
+     return { text: 'Password cannot be empty', type: 'danger', };
+      case 'null_email': return { text: 'Email cannot be empty', type: 'danger', };
+       default: return state;
+       }
+       };
 
-    
+const [message, dispatch] = useReducer(reducer, initMessage);
 
-    const reducer = (state, action) => {
-  switch (action) {
-    case 'success':
-      return {
-        text: "Added Successfully",
-        type: 'success'
-      };
-    case 'null_password':
-      return {
-        text: "Password cannot be empty",
-        type: 'danger'
-      };
-    case 'null_email':
-      return {
-        text: "Email cannot be empty",
-        type: 'danger'
-      };
-    default:
-      return state;
-  }
-};
+function AddData(e) { e.preventDefault();
 
-
-    const[message , dispatch] = useReducer(reducer,initMessage)
-
-    function AddData(e){
-        e.preventDefault()
-
-        if(password && email){
-            firebase.firestore().collection(userUID).add({
-                email: email,
-                password: password
-            })
-            setEmail('')
-            setPassword('')
-            dispatch('success')
-        }
-        if(password === '' && email){
-            dispatch('null_password')
-        }
-        if(email === '' && password){
-            dispatch('null_email')
-        }
-
-
-    }
-    
-    
-
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center',marginTop:'10%', minHeight: '100vh' }}>
-
-            {message.type? <h4>{message.text}</h4> : null}
-            <h1 style={{textAlign: 'center',marginTop:'-4%',marginRight:'-18%'}}> Add Password</h1>
-            <div style={{width: '50%' , marginLeft: '25px'}} > 
-            <Form>
-                
-                <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} style={{ width: '40%' }}/>
-
-                 </Form.Group>
-
-                 <Form.Group controlId="formBasicPassword">
-                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" value={password} onChange={e=> setPassword(e.target.value)} style={{ width: '40%' }}/>
-                 </Form.Group>
-                 <Button variant="primary" type="submit" onClick={(e)=> AddData(e)}>
-                 Submit
-                 </Button>
-                </Form>
-            </div>
-        </div>
-    )
+if (password && email) {
+  firebase.firestore().collection(userUID).add({
+    email: email,
+    password: password,
+  });
+  setEmail('');
+  setPassword('');
+  dispatch('success');
+}
+if (password === '' && email) {
+  dispatch('null_password');
+}
+if (email === '' && password) {
+  dispatch('null_email');
+}
 }
 
+return ( <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10%', minHeight: '100vh' }}>
+   {message.type ? ( <h4 style={{ justifyContent: 'center', alignSelf: 'center', marginTop: '-45%', marginRight: '-10%' }}>{message.text}</h4> ) : null}
+    <h1 style={{ textAlign: 'center', marginTop: '-1%', marginRight: '-18%' }}> Add Password</h1> 
+    <div style={{ width: '50%', marginLeft: '25px', marginTop: '4%' }}> 
+    <Form> 
+      <Form.Group controlId="formBasicEmail"> 
+      <Form.Label>Email address</Form.Label>
+       <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} style={{ width: '40%' }} /> </Form.Group>
+
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ width: '40%' }} />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={(e) => AddData(e)}>
+        Submit
+      </Button>
+    </Form>
+  </div>
+</div>
+); 
+}
 export default Add
